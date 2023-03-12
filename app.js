b9aff6767c3980331d39067fe56f2a4be8576987
thenabiev <sahinnabee@gmail.com>
2023-03-13 01:00:34 +0400

    express and nodemon initalized

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+
+    const photo={
+        id:1,
+        name:"Thumbnail",
+        description:"Lorem ipsum dolor sit amet"
+    }
+    
+    res.send(photo)
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server ${port} nömrəli portda çalışır`);
+});
